8a5bc3dfcfd9feb36e87756111887cfb10a9864a
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-11-03 17:22:44 +0500

    Added some animation to list

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ const random_button = document.getElementById("random_button")
 const box = document.querySelector(".box")
 
 random_button.addEventListener('click', () => {
-    box.innerHTML = ''
     random_button.setAttribute("disabled", '')
     fetch(URL)
         .then(res => res.json())
         .then(data => {
+            box.innerHTML = ''
             random_button.removeAttribute("disabled", '')
             data.forEach((e, i) => {
                 const name = document.createElement("p")
                 const div = document.createElement("div")
                 div.classList.add("wrapper")
                 name.classList.add("names")
-                name.textContent = (i + 1) + ": " + e.name
+                name.textContent = (i + 1) + " - " + e.name
+                div.addEventListener('click', (e) => {
+                    e.target.remove()
+                })
                 div.append(name)
                 box.append(div)
             })
